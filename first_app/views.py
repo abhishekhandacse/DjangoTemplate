096cb19dac693c4339b52f1e181e9392b6f469ea
abhishekhandacse <abhishekhandacse@gmail.com>
2017-10-16 17:32:47 +0530

    Error related to form fixed and encryption added

--- first_app/views.py
+++ first_app/views.py
@@ -12,9 +12,14 @@ def index(request):
 def form_name_view(request):
     form=forms.FormName()
     if request.method=='POST':
+        form=forms.FormName(request.POST)
         if form.is_valid():
             print("Validation Successful!!")
             print("NAME"+form.cleaned_data['name'])
             print("EMAIL"+form.cleaned_data['email'])
             print("TEXT"+form.cleaned_data['text'])
+        else:
+            print ("SOME ERROR OCCURED-1")
+    else:
+        print ("SOME ERROR OCCURED-2")
     return render (request,'first_app/form_page.html', {'form':form} )
